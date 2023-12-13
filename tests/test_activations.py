2541d1f27560bca3d2417b398ceb1b1f657cf7a5
Nathan Simpson <nsimpson@turing.ac.uk>
2023-12-13 15:56:40 +0000

    add tests

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from neural_network.activations import sigmoid
+from neural_network.activations import sigmoid, tanh
 
 
 # Test sigmoid function with scalar input
@@ -22,3 +22,24 @@ def test_sigmoid_array():
 def test_sigmoid_output_range():
     for x in np.linspace(-10, 10, 100):
         assert 0 <= sigmoid(x) <= 1
+
+
+# Test tanh function with scalar input
+def test_tanh_scalar():
+    assert tanh(0) == 0
+    assert tanh(100) > 0.99
+    assert tanh(-100) < -0.99
+
+
+# Test tanh function with numpy array input
+def test_tanh_array():
+    input_array = np.array([-1, 0, 1])
+    output_array = tanh(input_array)
+    expected_output = np.array([np.tanh(-1), np.tanh(0), np.tanh(1)])
+    assert np.allclose(output_array, expected_output)
+
+
+# Test tanh output range
+def test_tanh_output_range():
+    for x in np.linspace(-10, 10, 100):
+        assert -1 <= tanh(x) <= 1
